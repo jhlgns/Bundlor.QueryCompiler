26c6fbc9686e049d577539091a4804011166c23b
Jan Hahlganß <j.hahlganss@yesgate.de>
2023-01-12 07:13:32 +0100

    Initial commit

---
+++ Jhlgns.QueryCompiler.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Jhlgns.QueryCompiler.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Jhlgns.QueryCompiler/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Jhlgns.QueryCompiler;
+public class Class1
+{
+
+}
